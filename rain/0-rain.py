2b5967209d1fe9d2ca71c6cf1b95e835fd854297
Anesu Kafesu <a.kafesu@alustudent.com>
2023-10-05 22:45:33 +0200

    0

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,26 +1,48 @@
 #!/usr/bin/python3
 """Module for the rain function
 """
-def rain(walls):
+def rain(walls = []):
     """Calculates how much rain a series of walls can hold
     """
+    number_of_walls = len(walls)
 
     # Return 0 if there are no walls
-    if len(walls) == 0:
+    if number_of_walls == 0:
         return 0
     
-    # If there are walls
-    current_volume = 0
+    # Finding the highest wall to the left of each of wall
+    highest_left_walls = []
+    max_so_far = 0
+
+    for i in range(number_of_walls):
+        wall = walls[i]
+        if wall > max_so_far:
+            max_so_far = wall
+        
+        highest_left_walls.append(max_so_far)
+
+
+    # Finding the highest wall to the right of each wall
+    highest_right_walls = []
+    max_so_far = 0
+
+    for i in range(number_of_walls - 1, -1, -1):
+        wall = walls[i]
+        if wall > max_so_far:
+            max_so_far = wall
+        
+        highest_right_walls.append(max_so_far)
+    
     total_volume = 0
-    current_left_wall = 0
+    
+    # Calculate the amount of water held by each element
+    for i in range(number_of_walls):
+        highest_left_wall = highest_left_walls[i]
+        highest_right_wall = highest_right_walls[i]
+        wall_height = walls[i]
 
-    for wall in walls:
-        if wall >= current_left_wall:
-            total_volume += current_volume
-            current_left_wall = wall
-            current_volume = 0
-        else:
-            current_volume += current_left_wall - wall
+        volume = min(highest_left_wall, highest_right_wall) - wall_height
 
+        total_volume += volume
 
     return total_volume
